e329057654577d1f7e8168a4c4dc1e0b073b5bf2
jmbo <jmbo@users.noreply.github.com>
2025-07-03 20:14:57 -0400

    phase 2 revisions to modern JS coding

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,6 +1,8 @@
 //////////////////////// Let's Fix Some Bad Code: Part 1 Lesson /////////////////////////////
+//////////////////////// Let's Fix Some Bad Code: Part 2 Lesson /////////////////////////////
+'use strict';
 
-const budget = [
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,47 +11,60 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
+  // makes object immutable -- only the first level though, it's not a "deep freeze"
   jonas: 1500,
   matilda: 100,
+});
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// This is now a "pure function" -- does not mutate any outside objects / creates "side effects"
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
-const getLimit = user => spendingLimits?.[user] ?? 0;
+// we can use composing to crate just one function instead and replace this chaining operations below to occur at once
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+// function is now nice and pure
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
 
-  //   const lim = spendingLimits[user] ? spendingLimits[user] : 0;
-  // OR
-  //   const lim = spendingLimits?.[user] ?? 0;
-  // OR
-  //   const lim = getLimit(user);
-  // OR
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+// Impure function because it creates a side effect by having a console.log(), but that's ok!
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2)) // Emojis are 2 chars
+    .join(' / ');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
-
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) {
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : ''; // Emojis are 2 chars
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+  console.log(bigExpenses);
 };
 
-console.log(budget);
-logBigExpenses(1000);
+logBigExpenses(finalBudget, 1000);
